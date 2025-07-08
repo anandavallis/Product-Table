a12a8eafd86afa4cf73020f21f52c3c595debc04
anandavallis <sanandavalli7@gmail.com>
2025-07-08 15:11:47 +0530

    Create App.jsx

---
+++ product-table/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProductTable from './components/ProductTable';
+import './App.css';
+
+
+const App = () => {
+  return <ProductTable />;
+};
+
+export default App;
